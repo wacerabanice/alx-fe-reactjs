ed52db901be00f224f2b4d94731ada381da01394
Wacera <winniebanice@gmail.com>
2025-10-27 12:45:15 +0300

    update the react app

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,8 +29,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
+      <div>
+        <WelcomeMessage />
+      </div>
+  
+</>
+  );
+      
 }
 
-export default App
+export default App;
