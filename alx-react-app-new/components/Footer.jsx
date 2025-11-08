114d96fa61bbab2d903a676a52e7fe7510da1762
Wacera <winniebanice@gmail.com>
2025-11-08 12:47:00 +0300

    Add new react app

---
+++ alx-react-app-new/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+  return(
+    <footer style={{ backgroundColor: 'lightgreen', color: 'white', textAlign: 'center' }}>
+  <p>© 2023 City Lovers</p>
+</footer>
+  );
+}
+
+
+export default Footer;
---
+++ alx-react-app-new/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+  return(
+    <header style={{ backgroundColor: 'lightgreen', color: 'white', textAlign: 'center' }}>
+  <h1>My Favorite Cities</h1>
+</header>
+  );
+}
+
+
+export default Header;
+
