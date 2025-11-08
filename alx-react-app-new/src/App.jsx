d0e711145a750be4eb0bf988d5701672944c8f34
Wacera <winniebanice@gmail.com>
2025-11-08 12:50:01 +0300

    move component file into src

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 import './App.css'
 import WelcomeMessage from '/components/WelcomeMessage'
 import Header from '/components/Header'
-import UserProfile from '../components/UserProfile'
-import Footer from '../components/Footer'
-import Counter from '../components/Counter'
+import UserProfile from './components/UserProfile'
+import Footer from './components/Footer'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
