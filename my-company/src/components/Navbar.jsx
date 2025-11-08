a11a0705aecd431df451d3564a2dac0a75695f1b
Wacera <winniebanice@gmail.com>
2025-11-08 15:09:58 +0300

    add mycompany app

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ backgroundColor: 'navy', padding: '10px', color: 'white' }}>
+      <Link to="/" style={{ margin: '0 10px', color: 'white' }}>Home</Link>
+      <Link to="/about" style={{ margin: '0 10px', color: 'white' }}>About</Link>
+      <Link to="/services" style={{ margin: '0 10px', color: 'white' }}>Services</Link>
+      <Link to="/contact" style={{ margin: '0 10px', color: 'white' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
