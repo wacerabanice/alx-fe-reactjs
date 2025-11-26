d46c54bdac804e0c923dc48056a1832a97c2e5d7
Wacera <winniebanice@gmail.com>
2025-11-26 21:37:32 +0300

    update

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -20,7 +20,7 @@ function Search() {
       const data = await fetchUserData(username);
       setUser(data);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
