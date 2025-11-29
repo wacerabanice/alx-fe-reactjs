55b9b414ceb254a223306748c2428e1d8f76e012
Wacera <winniebanice@gmail.com>
2025-11-29 18:04:51 +0300

    update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
+        className=" sm:w-24 sm:h-24 md:w-36 md:h-36 md:w-36 md:h-36 mx-auto transform hover:scale-110 transition-transform duration-300 ease-in-out"
       />
       <h1 className="text-lg md:text-xl text-blue-800 my-4">
         John Doe
