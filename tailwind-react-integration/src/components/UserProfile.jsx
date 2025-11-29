465cfaec3f23db97061c37d4e2094c23f1d4cc71
Wacera <winniebanice@gmail.com>
2025-11-29 17:56:36 +0300

    update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out text-center">
+    <div className="user-profile bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto transform hover:scale-110 transition-transform duration-300 ease-in-out"
+        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500 transition-colors duration-300">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">
         John Doe
       </h1>
       <p className="text-gray-600 text-sm md:text-base">
@@ -17,4 +17,3 @@ function UserProfile() {
 }
 
 export default UserProfile;
-
