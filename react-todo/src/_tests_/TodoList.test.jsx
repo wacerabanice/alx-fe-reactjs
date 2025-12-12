68d86e6ec507f4abca06a0448460c3cc2a92691b
Wacera <winniebanice@gmail.com>
2025-12-12 13:10:59 +0300

    update

--- react-todo/src/_tests_/TodoList.test.jsx
+++ react-todo/src/_tests_/TodoList.test.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
-import TodoList from "../components/TodoList.jsx";
+import TodoList from "../components/TodoList.js";
 
 describe("TodoList Component", () => {
   test("renders initial todos", () => {
