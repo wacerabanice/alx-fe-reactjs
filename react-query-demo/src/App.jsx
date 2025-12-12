ecb70c1026faf5dc81f537470e4833729524cc8c
Wacera <winniebanice@gmail.com>
2025-12-12 12:22:41 +0300

    update

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import RegistrationForm from "./components/RegistrationForm";
+import PostsComponent from "./components/PostsComponent";
 
 // Create a single QueryClient instance
 const queryClient = new QueryClient();
@@ -9,8 +10,15 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div style={{ padding: "20px" }}>
-        <h1>React Query + Form Example</h1>
+        <h1>React Query Demo</h1>
+
+        {/* Registration Form */}
         <RegistrationForm />
+
+        <hr style={{ margin: "20px 0" }} />
+
+        {/* Posts Fetching Component */}
+        <PostsComponent />
       </div>
     </QueryClientProvider>
   );
